2e9061f163bedabbe7bce713f90a4260c87f38db
Louveau Clément <157509225+valak74200@users.noreply.github.com>
2025-07-22 18:08:00 +0200

    Fix Next.js configuration errors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.pexels.com'],
+  },
+  swcMinify: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
   },
 }
 
